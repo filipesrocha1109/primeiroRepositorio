ac00369bd0548c1472496256b3a270a28dd38507
filipesrocha1109 <fsrbillie@gmail.com>
2019-05-09 12:38:11 -0300

    ...

---
+++ exercicios-js/esnext/map.js
@@ -0,0 +1,21 @@
+const tecnologias = new Map()
+tecnologias.set('react', {framework : false})
+tecnologias.set('angular',{framework : true})
+
+console.log(tecnologias.get('react').framework)
+console.log(tecnologias.get('angular').framework)
+
+const chavesVariadas = new Map([
+    [function(){}, 'função'],
+    [{}, 'objeto'],
+    [123, 'numero'],
+])
+
+chavesVariadas.forEach((vl,ch)=>{
+    console.log(ch,vl)
+})
+
+console.log(chavesVariadas.has(123)) // esta presente no Map
+chavesVariadas.delete(123)
+console.log(chavesVariadas.has(123))
+console.log(chavesVariadas.size)
